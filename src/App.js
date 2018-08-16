57112fade1fa03e35a03c855ff1d1e3c0cb28f38
Tim Engler <timengler@atv-tim-engler.local>
2018-08-16 11:25:53 -0700

    backing that shiiiiiit up

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './concern_printfile_default_15oz_grande.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Concerned Reader</h1>
+        </header>
+        <p className="App-intro">
+          
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
